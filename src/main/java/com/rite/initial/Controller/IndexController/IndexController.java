466cdb18236c790e35c6e58badd4fbbc875c5048
邓浅析 <windowsphone520t@live.com>
2018-06-22 11:54:45 +0800

    Initial commit

---
+++ src/main/java/com/rite/initial/Controller/IndexController/IndexController.java
@@ -0,0 +1,15 @@
+package com.rite.initial.Controller.IndexController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String start(){
+        return "123";
+    }
+
+}
---
+++ src/main/java/com/rite/initial/InitialApplication.java
@@ -0,0 +1,12 @@
+package com.rite.initial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialApplication.class , args);
+    }
+}
